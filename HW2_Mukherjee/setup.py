95188d2f1730a1dc829ae9796de23beb5f19d41f
iamshnoo <mukherjee.anjishnu@gmail.com>
2022-11-04 12:34:58 -0400

    Update gitignore

--- HW2_Mukherjee/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="hw2",
-    version="1.0",
-    description="CS 584 Fall 2022 Homework 2 Anjishnu Mukherjee G01373990",
-    author="Anjishnu Mukherjee",
-    author_email="amukher6@gmu.edu",
-)
